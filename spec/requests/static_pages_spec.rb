5d9cfa0ec8b1a95bd3de4b16de49111c11634654
Brian Aznar <baznar@gmail.com>
2012-02-26 18:32:08 -0500

    added guard and spork to testing framework

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "StaticPages" do
     end
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title',:text => "#{base_title} | Home")
     end
   end
 
@@ -20,7 +22,7 @@ describe "StaticPages" do
     end
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title',:text => "#{base_title} | Help")
     end
   end
 
@@ -29,10 +31,20 @@ describe "StaticPages" do
       visit '/static_pages/about'
       page.should have_selector('h1',:text => "About Us")
     end
-  end
     it "should have the right title 'About'" do
       visit '/static_pages/about'
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title',:text => "#{base_title} | About Us")
     end
-  
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1',:text => "Contact")
+    end
+    it "should have the right title 'contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',:text => "#{base_title} | Contact")
+    end
+  end
 end
