82ba1eae2170e6dda7d29eb4368498e6f46a4e2e
Brian Aznar <baznar@gmail.com>
2012-03-09 18:57:57 -0500

    make a basic user model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,25 +8,25 @@ describe "StaticPages" do
   
   describe "Home page" do
     before { visit root_path }
-    it { page.should have_selector('h1',text: "Sample App") }
-    it { page.should have_selector('title',text: full_title('Home')) }
+    it { should have_selector('h1',text: "Sample App") }
+    it { should have_selector('title',text: full_title('Home')) }
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { page.should have_selector('h1',text:  "Help") } 
-    it { page.should have_selector('title',text: full_title('Help')) } 
+    it { should have_selector('h1',text:  "Help") } 
+    it { should have_selector('title',text: full_title('Help')) } 
   end
 
   describe "About page" do
     before { visit about_path }
-    it { page.should have_selector('h1',text: "About Us") } 
-    it { page.should have_selector('title',text: full_title('About Us')) } 
+    it { should have_selector('h1',text: "About Us") } 
+    it { should have_selector('title',text: full_title('About Us')) } 
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { page.should have_selector('h1',text: "Contact") } 
-    it { page.should have_selector('title',text: full_title('Contact')) } 
+    it { should have_selector('h1',text: "Contact") } 
+    it { should have_selector('title',text: full_title('Contact')) } 
   end
 end
